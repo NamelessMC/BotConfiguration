56d9f054f1162e23ab3f278e8772109790a483f1
Robin <robinslot@hotmail.nl>
2022-06-26 22:41:53 +0200

    Update time out autoresponse

--- autoresponse.js
+++ autoresponse.js
@@ -382,6 +382,8 @@ module.exports = [
         keywords: [
             ["IOException: Read timed out"],
             ["IOException: Connect timed out"],
+            ["HttpHeadersTimeoutException"],
+            ["couldn't receive headers on time"],
         ],
         response: {
             "title": "Plugin or Nameless-Link times out while waiting for response",
@@ -392,7 +394,8 @@ module.exports = [
                 "1. Invalid address: if the address is invalid, the plugin or Nameless-Link is never going to get a response",
                 "2. Firewall: if a firewall is blocking requests from the plugin or Nameless-Link, your website will never send a response and as a result, no response will be received. In case of Nameless-Link, you may need to ask your hosting provider to whitelist requests from 135.125.205.25.",
                 "3. Slow web server: ensure the database server and web server are on the same machine, otherwise it might not be able to produce API responses quickly enough due to query latency.",
-                "4. Update checker in v2-pr12 and older: In older versions, the API info endpoint sometimes checked for a NamelessMC update, delaying the response. This update check has been removed in v2-pr13.",
+                "4. Discord group sync: due to a bug in v2-pr13, Discord group sync can be *extremely* slow, delaying web requests for far too long. Please remove any Discord group sync rules in StaffCP > Configuration > API > Group Sync",
+                "5. Update checker in v2-pr12 and older: In older versions, the API info endpoint sometimes checked for a NamelessMC update, delaying the response. This update check has been removed in v2-pr13.",
             ]
         }
     },
