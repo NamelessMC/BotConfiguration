7f85024ed29f969b6887c8d0761b52c5509e9e3b
Robin Slot <robin+github@rkslot.nl>
2023-03-03 23:24:55 +0100

    Remove outdated IP address

--- autoresponse.js
+++ autoresponse.js
@@ -386,10 +386,10 @@ module.exports = [
             "body": [
                 "This error means the website hasn't sent a response to an API call in a reasonable amount of time and the connection was aborted. Some possible causes are listed below.",
                 "",
-                "1. Discord group sync: Due to several bugs, Discord group sync can be *extremely* slow, delaying web requests for far too long. Please remove any Discord group sync rules in StaffCP > Configuration > API > Group Sync. This will be fixed in 2.1.0.",
+                "1. Discord group sync (most likely): Due to several bugs, Discord group sync can be *extremely* slow, delaying web requests for far too long. Please remove any Discord group sync rules in StaffCP > Configuration > API > Group Sync. This will be fixed in 2.1.0.",
                 "2. Invalid address: if the address is invalid, the plugin or Nameless-Link is never going to get a response",
-                "3. Firewall: if a firewall is blocking requests from the plugin or Nameless-Link, your website will never send a response and as a result, no response will be received. In case of Nameless-Link, you may need to ask your hosting provider to whitelist requests from 135.125.205.25.",
-                "4. Slow web server: ensure the database server and web server are on the same machine, otherwise it might not be able to produce API responses quickly enough due to query latency.",
+                "3. Slow web server: ensure the database server and web server are on the same machine, otherwise it might not be able to produce API responses quickly enough due to query latency.",
+                "4. Firewall (unlikely): if a firewall is blocking requests from the plugin or Nameless-Link, your website will never send a response and as a result, no response will be received.",
                 "5. (only for v2-pr12 and older) One API endpoint endpoint sometimes checked for a NamelessMC update, delaying the response. This update check has been removed in later versions. Please update.",
             ]
         }
